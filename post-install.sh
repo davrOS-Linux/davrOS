2d0338b37eefd058153b28ca6c10435d2cfca266
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-05-24 11:54:03 +0100

    add nvchad

--- post-install.sh
+++ post-install.sh
@@ -3,3 +3,6 @@
 echo "Commencing Stage 1: Package Installation"
 pacman -Syu - < pkg.txt
 echo "Stage 1 Complete: Package Installation"
+echo "Commencing Stage 2: Configuration"
+echo "Stage 2.1: Configuring neovim"
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
