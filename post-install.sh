f5dd57b14da0096077c326d84c004bda10d027a2
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-05-24 22:03:29 +0100

    add more space (m o r e   b r e a t h a b l e)

--- post-install.sh
+++ post-install.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
 echo "Commencing Stage 0: Repository Configuration"
+
 # Add chaotic AUR (because why not, right?... right?)
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo "[chaotic-aur]
 Include = /etc/pacman.d/chaotic-mirrorlist " >> /etc/pacman.conf
+
 echo "Commencing Stage 1: Package Installation"
+
 # Install all the packages listed in the pkg.txt file using pacman
 pacman -Syu - < pkg.txt
-echo "Stage 1 Complete: Package Installation"
+
 echo "Commencing Stage 2: Configuration"
 echo "Stage 2.1: Configuring neovim"
+
 # Install nvchad (because its epic)
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
