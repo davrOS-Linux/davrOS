f0f8bfd9c640a8182176b3c2cc402df81bb5b331
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-07-22 23:16:07 +0100

    reword

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "[davrOS]: stage 1: Package Installation"
+echo "[davrOS]: stage 1: Package Upgrades"
 
 # install all the packages listed in the pkg.txt file using pacman
 pacman -Syu --needed - < pkg.txt
